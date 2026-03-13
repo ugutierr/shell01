a129af0bb007b83051838fff07f69c1eee8fd882
Unai <184846045+Gutifx@users.noreply.github.com>
2026-03-13 20:45:46 +0100

    ex07 corregido

--- ex07/r_dwssap.sh
+++ ex07/r_dwssap.sh
@@ -1,1 +1,1 @@
-cat /etc/passwd | cut -d ":" -f 1 | awk 'NR%2==0' | rev | sort -r | awk "NR>=$FT_LINE1 && NR<=$FT_LINE2" | paste -sd "," | sed 's/,/, /g'
+cat /etc/passwd | cut -d ":" -f 1 | awk 'NR%2==0' | rev | sort -r | awk "NR>=$FT_LINE1 && NR<=$FT_LINE2" | paste -sd "," | sed 's/,/, /g' | awk '{printf "%s.", $0}'
